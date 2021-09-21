61149905539ebf0cad076c99b4464eaee636267c
kevinpvo <kevin.p.vo@colorado.edu>
2021-09-21 13:53:35 -0600

    updated cpp

--- test.cpp
+++ test.cpp
@@ -4,11 +4,8 @@
 #include <vector>
 
 TEST_CASE ( "Factorials are computed", "[factorial]") {
-  REQUIRE( Factorial(0) == 1 );
-  REQUIRE( Factorial(1) == 1 );
-  REQUIRE( Factorial(2) == 2 );
-  REQUIRE( Factorial(3) == 6 );
-  REQUIRE( Factorial(10) == 3628800 );
+	std::vector<int> v{1,1,1,1};	
+	REQUIRE( Product(v) == 1 );
 }
 
 TEST_CASE("incrementing values in integer vector", "[addN]"){
